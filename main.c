437cfd4be47675a80fbfa8ba0c9056c3c7ee9504
Aliberk Sandıkçı <git@asandikci.com>
2025-12-23 23:12:22 +0300

    Add error handling for file operations

--- main.c
+++ main.c
@@ -1,11 +1,25 @@
 #include <stdio.h>
 
-int main(void)
+int main(int)
 {
     FILE *kfptr,*dfptr,*ofptr;
     kfptr=fopen("kodlar.txt","r");
     dfptr=fopen("d.txt","r");
     ofptr=fopen("orijinal.txt","w");
+
+    if (!dfptr) {
+        printf("Hata: d.txt bulunamadi!\n");
+        return 1;
+    }
+    if (!kfptr) {
+        printf("Hata: kodlar.txt bulunamadi!\n");
+        return 1;
+    }
+    if (!ofptr) {
+        printf("Hata: orijinal.txt olusturulamadi!");
+        return 1;
+    }
+
     int sayac=0,i=0,kod;
     while(!feof(dfptr))
     {
@@ -22,7 +36,11 @@ int main(void)
     while(!feof(kfptr))
     {
         fscanf(kfptr,"%d ",&kod);
-        fprintf(ofptr,"%c",karakterler[kod-1]);
+        if(kod > 0 && kod <= sayac) {
+            fprintf(ofptr,"%c",karakterler[kod-1]);   
+        } else {
+            printf("%d indisi bulunmadigi icin atlandi\n", kod);
+        }
     }
     fclose(kfptr);
     fclose(dfptr);
